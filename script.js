686f8e4bcd9c2bd0086f6416bfc46374a7bd9fe7
Damjan83 <damjanpokrajac83@gmail.com>
2021-02-08 19:03:08 +0100

    add my new project

---
+++ script.js
@@ -0,0 +1,12 @@
+function closeMenu(){
+    document.getElementById("navbar").style.height = "0%";
+}
+function openMenu(){
+    document.getElementById("navbar").style.height = "100%";
+}
+
+
+
+
+
+
